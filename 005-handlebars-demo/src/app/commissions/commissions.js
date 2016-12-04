d0bc272e77106fcd2a3263eda5bb6d59ecd03a71
Dan Dohotaru <dan.dohotaru@gmail.com>
2016-12-04 23:19:51 +0100

    rework the commissions page to fit the layout

--- 005-handlebars-demo/src/app/commissions/commissions.js
+++ 005-handlebars-demo/src/app/commissions/commissions.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
+
+  var loader = new Loader();
+
+  // Header
+  var user = {
+    firstName: "John",
+    lastName: "Doe"
+  };
+  var header = new HeaderComponent(loader);
+  header.init(user).then(function (html) {
+    $("#header").html(html);
+  });
+
+  // Menu
+  var menu = new MenuComponent(loader);
+  menu.init().then(function (html) {
+    $("#menu").html(html);
+  });
+
+  // Footer
+  var footer = new FooterComponent(loader);
+  footer.init().then(function (html) {
+    $("#footer").html(html);
+  });
+
+  // Content
   $('[data-toggle="left-canvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 });
-
