d92354f7515a6d1e5ec40760989b5555c036e8b0
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-04-18 00:25:30 +0300

    add basic and load more scenarios

---
+++ src/more/more.js
@@ -0,0 +1,19 @@
+var colcade = new Colcade('.grid', {
+    columns: '.grid-col',
+    items: '.grid-item'
+});
+
+document.querySelector('.load-more-button').onclick = function () {
+    colcade.append([
+        makeItem(), makeItem(), makeItem(), makeItem(), makeItem(),
+        makeItem(), makeItem(), makeItem(), makeItem(), makeItem()
+    ]);
+};
+
+function makeItem() {
+    var item = document.createElement('div');
+    item.classList.add('grid-item');
+    var height = Math.floor(Math.random() * 400 + 200);
+    item.innerHTML = '<img src="http://lorempixel.com/400/' + height + '" />';
+    return item;
+}
