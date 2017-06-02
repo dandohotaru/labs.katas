5fd19648451389842c60340985ac14fada94aacb
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-06-02 15:30:45 +0200

    initial setup with dist and serve via webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require('webpack');
+
+var config = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './app.js',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './src'),
+    }
+};
+
+module.exports = config;
