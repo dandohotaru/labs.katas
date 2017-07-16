49fbaa5853f2f7660941e35681dab8f628900fa8
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-07-16 20:49:01 +0200

    implement sample based community answer

---
+++ src/pages/page3/about.md
@@ -0,0 +1,2 @@
+Inspiration
+https://stackoverflow.com/questions/44136087/offcanvas-sidebar-with-bootstrap-3
---
+++ src/pages/page3/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    console.log("ready");
+
+    $('.off-canvas-toggle').on('click', function (event) {
+        event.preventDefault();
+        $('body').toggleClass('off-canvas-active');
+    });
+
+    $(document).on('mouseup touchend', function (event) {
+        var offCanvas = $('.off-canvas')
+        if (!offCanvas.is(event.target) && offCanvas.has(event.target).length === 0) {
+            $('body').removeClass('off-canvas-active')
+        }
+    });
+});
