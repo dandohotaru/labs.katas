b3ac6e5f68b7a6891b77f55603d1358307758c7a
Dan Dohotaru <dan.dohotaru@gmail.com>
2017-08-06 13:45:02 +0200

    work in progress on flexible off canvas samples

--- src/pages/page0/index.js
+++ src/pages/page0/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-    $('[data-toggle="offcanvas"]').click(function () {
+    // $('[data-toggle="offcanvas"]').click(function () {
+    //     $('.row-offcanvas').toggleClass('active')
+    // });
+
+    $('#toggleLeft').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+
+    $('#toggleRight').click(function () {
         $('.row-offcanvas').toggleClass('active')
     });
 });
