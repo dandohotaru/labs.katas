ea192cce87df49b8a2f2ff610ec11ee1fef618b9
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-06-18 22:47:18 +0200

    rely on json defined data to populate network and timeline

--- src/app/features/kik0002/network.component.js
+++ src/app/features/kik0002/network.component.js
@@ -18,25 +18,10 @@ export class NetworkComponent {
   }
 
   load(element) {
-    var nodes = new DataSet([
-      { id: 1, label: 'Node 1' },
-      { id: 2, label: 'Node 2' },
-      { id: 3, label: 'Node 3' },
-      { id: 4, label: 'Node 4' },
-      { id: 5, label: 'Node 5' }
-    ]);
-
-    var edges = new DataSet([
-      { from: 1, to: 3 },
-      { from: 1, to: 2 },
-      { from: 2, to: 4 },
-      { from: 2, to: 5 },
-      { from: 3, to: 3 }
-    ]);
 
     var data = {
-      nodes: nodes,
-      edges: edges
+      nodes: new DataSet(records.nodes),
+      edges: new DataSet(records.edges),
     };
 
     var options = {
