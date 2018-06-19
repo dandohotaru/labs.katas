d102176913a45f59fce65b948407306d802bb06a
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-06-19 07:39:13 +0200

    implement support for custom menu buttons

--- src/app/features/kik0003/timeline.component.js
+++ src/app/features/kik0003/timeline.component.js
@@ -6,10 +6,12 @@ import records from "./records.json";
 import { DataSet, Timeline } from 'vis/index-timeline-graph2d';
 import 'vis/dist/vis-timeline-graph2d.min.css';
 
-export class TimelineComponent  {
-  
+export class TimelineComponent {
+
+  timeline;
+
   init(selector) {
-    
+
     var container = document.querySelector(selector);
     container.innerHTML = template();
 
@@ -19,6 +21,29 @@ export class TimelineComponent  {
 
   load(element) {
     var data = new DataSet(records);
-    var timeline = new Timeline(element, data, {});
+    this.timeline = new Timeline(element, data, {});
+
+    document.getElementById('zoomIn').onclick = () => this.zoom(0.2);
+    document.getElementById('zoomOut').onclick = () => this.zoom(-0.2);
+    document.getElementById('moveLeft').onclick = () => this.move(0.2);
+    document.getElementById('moveRight').onclick = () => this.move(-0.2);
+  }
+
+  zoom(percentage) {
+    if (percentage >= 0) {
+      this.timeline.zoomIn(percentage);
+    } else {
+      this.timeline.zoomOut(Math.abs(percentage));
+    }
+  }
+
+  move(percentage) {
+    var range = this.timeline.getWindow();
+    var interval = range.end - range.start;
+
+    this.timeline.setWindow({
+      start: range.start.valueOf() - interval * percentage,
+      end: range.end.valueOf() - interval * percentage
+    });
   }
 }
