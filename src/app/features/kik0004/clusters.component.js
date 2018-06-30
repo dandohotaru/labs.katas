374e5d4d649a94e0dae83b5d0e19bcf8841f930d
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-07-01 00:13:41 +0200

    implement clusterizer as a dedicated service

--- src/app/features/kik0004/clusters.component.js
+++ src/app/features/kik0004/clusters.component.js
@@ -6,11 +6,13 @@ import 'vis/dist/vis-timeline-graph2d.min.css';
 import ClustersData from "./records.json";
 import ClustersView from "./clusters.component.hbs";
 import ClustersStyles from "./clusters.component.css";
+import { ClustersService } from "./clusters.service";
 
 export class ClustersComponent {
 
   timeline;
   records;
+  service;
 
   init(selector) {
 
@@ -32,7 +34,7 @@ export class ClustersComponent {
     let options = {
       template: (item) => {
         return item.count
-          ? ("<span>" + item.count + " évenement" + ((item.count > 1)
+          ? ("<span>" + item.count + " event" + ((item.count > 1)
             ? 's'
             : '') + "</span>")
           : item.content;
@@ -46,15 +48,14 @@ export class ClustersComponent {
       options
     );
 
-    var clusters = new Clusteriser(this.timeline);
-    this.timeline.setItems(clusters.getClusters(items));
-
-    this.timeline.on('rangechanged', ()=> {
-      this.timeline.setItems(clusters.getClusters(items));
-    });
+    this.service = new ClustersService(this.timeline);
+    this.timeline.setItems(this.service.clusters(items));
   }
 
   listen() {
+    document.getElementById('zoomIn').onclick = () => this.zoom(0.2);
+    document.getElementById('zoomOut').onclick = () => this.zoom(-0.2);
+
     this.timeline.on('select', (properties) => {
     });
 
@@ -62,128 +63,19 @@ export class ClustersComponent {
     });
 
     this.timeline.on('rangechanged', (properties) => {
+      let items = this.records['items'];
+      this.timeline.setItems(this.service.clusters(items));
     });
 
     this.timeline.on('click', (properties) => {
     });
-
-    this.timeline.on('doubleClick', (properties) => {
-    });
-
-    this.timeline.on('contextmenu', (properties) => {
-    });
-
-    this.timeline.on('mouseDown', (properties) => {
-    });
-
-    this.timeline.on('mouseUp', (properties) => {
-    });
-  }
-}
-
-var Clusteriser = function (timeline) {
-  this.timeline = timeline;
-
-  this.getDiffDate = function (date1, date2) {
-    var second = 1000;
-    var minute = second * 60;
-    var hour = minute * 60;
-    var day = hour * 24;
-    var month = day * 31;
-    var year = month * 12;
-
-    // Convert both dates to milliseconds
-    var diff = date2.getTime() - date1.getTime();
-
-    // Convert back to components and return
-    return {
-      years: Math.round(diff / year),
-      months: Math.round(diff / month),
-      days: Math.round(diff / day),
-      hours: Math.round(diff / hour),
-      minutes: Math.round(diff / minute)
-    };
   }
 
-  //Get the scale between two date
-  //We can use the scale information from timeline api in order to know how to cluster data
-  this.getScale = function (start, end) {
-    var diff = this.getDiffDate(start, end);
-
-    if (diff.months > 12 * 2) {
-      return 'year';
+  zoom(percentage) {
+    if (percentage >= 0) {
+      this.timeline.zoomIn(percentage);
+    } else {
+      this.timeline.zoomOut(Math.abs(percentage));
     }
-    else if (diff.days > 31) {
-      return 'month';
-    }
-    else if (diff.hours > 24) {
-      return 'day';
-    }
-    else if (diff.minutes > 60) {
-      return 'hour';
-    }
-    else {
-      return 'second';
-    }
-  }
-
-  this.getClusters = function (dataItems) {
-    var timelineWindow = this.timeline.getWindow();
-
-    var scale = this.getScale(timelineWindow.start, timelineWindow.end);
-
-    //var group = _.toArray(_.groupBy(dataItems, 'group'));;
-    var group = dataItems
-      .reduce((output, current) => {
-        var found = output.find(p => p.key == current.group);
-        if (found) {
-          found.items.push(current);
-        }
-        else {
-          found = {
-            key: current.group,
-            items: [current],
-          };
-          output.push(found);
-        }
-
-        return output;
-      }, [])
-      .map(p => {
-        return p.items;
-      });
-
-
-    var result = {};
-
-    var formatByScale = {
-      year: 'Y',
-      month: 'Y-M',
-      weekday: 'Y-M-week-W',
-      day: 'Y-M-day-D',
-      hour: 'Y-M-D-H',
-      second: 'Y-M-D-H-m'
-    };
-
-    group.forEach(function (items, i) {
-      items.forEach(function (item, j) {
-        var index = moment(item.start).format(formatByScale[scale]) + '-group' + item.group;
-
-        if (!result[index]) {
-          result[index] = {
-            count: 1,
-            items: [],
-            group: item.group,
-            start: item.start
-          };
-        }
-
-        result[index].items.push(item);
-        result[index].count++;
-      })
-    });
-
-    var temp = Object.keys(result).map(i => result[i]);
-    return temp;
   }
 }
---
+++ src/app/features/kik0004/clusters.service.js
@@ -0,0 +1,112 @@
+import moment from 'moment';
+
+export class ClustersService {
+
+  timeline;
+
+  constructor(timeline) {
+    this.timeline = timeline;
+  }
+
+  delta(start, end) {
+    var second = 1000;
+    var minute = second * 60;
+    var hour = minute * 60;
+    var day = hour * 24;
+    var month = day * 31;
+    var year = month * 12;
+
+    // Convert both dates to milliseconds
+    var diff = end.getTime() - start.getTime();
+
+    // Convert back to components and return
+    return {
+      years: Math.round(diff / year),
+      months: Math.round(diff / month),
+      days: Math.round(diff / day),
+      hours: Math.round(diff / hour),
+      minutes: Math.round(diff / minute)
+    };
+  }
+
+  scale(start, end) {
+
+    // Use the scale information from timeline api in order to know how to cluster data
+    var diff = this.delta(start, end);
+
+    if (diff.months > 12 * 2) {
+      return 'year';
+    }
+    else if (diff.days > 31) {
+      return 'month';
+    }
+    else if (diff.hours > 24) {
+      return 'day';
+    }
+    else if (diff.minutes > 60) {
+      return 'hour';
+    }
+    else {
+      return 'second';
+    }
+  }
+
+  clusters(data) {
+    var range = this.timeline.getWindow();
+
+    var scale = this.scale(range.start, range.end);
+
+    var group = data
+      .reduce((output, current) => {
+        var found = output.find(p => p.key == current.group);
+        if (found) {
+          found.items.push(current);
+        }
+        else {
+          found = {
+            key: current.group,
+            items: [current],
+          };
+          output.push(found);
+        }
+
+        return output;
+      }, [])
+      .map(p => {
+        return p.items;
+      });
+
+
+    var result = {};
+
+    var formatByScale = {
+      year: 'Y',
+      month: 'Y-M',
+      weekday: 'Y-M-week-W',
+      day: 'Y-M-day-D',
+      hour: 'Y-M-D-H',
+      second: 'Y-M-D-H-m'
+    };
+
+    group.forEach((items, i) => {
+      items.forEach((item, j) => {
+        var index = moment(item.start).format(formatByScale[scale]) + '-group' + item.group;
+
+        if (!result[index]) {
+          result[index] = {
+            count: 1,
+            items: [],
+            group: item.group,
+            start: item.start
+          };
+        }
+
+        result[index].items.push(item);
+        result[index].count++;
+      })
+    });
+
+    var temp = Object.keys(result).map(i => result[i]);
+    return temp;
+  }
+}
