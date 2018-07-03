f654b281a3df82135afdcb3d7354956f1f55260c
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-07-03 09:41:01 +0200

    simplify the classes for the offcanvas

--- src/app/features/kik0005/example.js
+++ src/app/features/kik0005/example.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
-    $('.row-offcanvas').toggleClass('active')
+  $('[data-toggle="canvas"]').click(function () {
+    $('.offcanvas').toggleClass('active')
   });
 });
