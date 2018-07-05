6676031934bfe6f6c167ac455147fec4ac5e3389
Dan Dohotaru <dan.dohotaru@gmail.com>
2018-07-05 23:27:46 +0200

    fix styles overlapping due to lack of encapsulation. code cleanup

--- src/app/features/kik0004/clusters.component.js
+++ src/app/features/kik0004/clusters.component.js
@@ -15,31 +15,31 @@ export class ClustersComponent {
   service;
 
   init(selector) {
-
     this.records = ClustersData;
 
     let container = document.querySelector(selector);
     container.innerHTML = ClustersView(this);
 
-    var element = container.querySelector(".timeline");
+    var element = container.querySelector(".clusters");
     this.load(element);
     this.listen();
   }
 
   load(element) {
 
-    var groups = this.records['groups'];
+    var groups = this.records['groups'].filter(p=>p.id == 1);
     var items = this.records['items'];
 
     let options = {
       stack: true,
+      height: '600px',
       orientation: 'top',
       showCurrentTime: true,
       moveable: true,
-      min: new Date(2016, 0, 1),
-      max: new Date(2017, 0, 1),
-      start: new Date(2016, 8, 1),
-      end: new Date(2016, 10, 1),
+      min: new Date(2018, 0, 1),
+      max: new Date(2019, 0, 1),
+      start: new Date(2018, 8, 1),
+      end: new Date(2018, 9, 1),
       zoomMin: moment.duration(1, 'days').asMilliseconds(),
       zoomMax: moment.duration(6, 'months').asMilliseconds(),
       template: (item, element, data) => {
@@ -58,14 +58,33 @@ export class ClustersComponent {
   }
 
   listen() {
-    document.getElementById('zoomIn').onclick = () => this.zoom(0.2);
-    document.getElementById('zoomOut').onclick = () => this.zoom(-0.2);
+    document.getElementById('zoomIn').onclick = () => {
+      this.zoom(0.2);
+    };
 
-    document.getElementById('scaleDay').onclick = () => { this.timeline.setOptions({ timeAxis: { scale: 'day', step: 1 }, }); };
-    document.getElementById('scaleWeek').onclick = () => { this.timeline.setOptions({ timeAxis: { scale: 'day', step: 7 }, }); };
-    document.getElementById('scaleMonth').onclick = () => { this.timeline.setOptions({ timeAxis: { scale: 'month', step: 1 }, }); };
-    document.getElementById('scaleQuarter').onclick = () => { this.timeline.setOptions({ timeAxis: { scale: 'month', step: 3 }, }); };
-    document.getElementById('scaleYear').onclick = () => { this.timeline.setOptions({ timeAxis: { scale: 'year', step: 1 }, }); };
+    document.getElementById('zoomOut').onclick = () => {
+      this.zoom(-0.2);
+    };
+
+    document.getElementById('scaleDay').onclick = () => { 
+      this.timeline.setOptions({ timeAxis: { scale: 'day', step: 1 }}); 
+    };
+
+    document.getElementById('scaleWeek').onclick = () => { 
+      this.timeline.setOptions({ timeAxis: { scale: 'day', step: 7 }}); 
+    };
+
+    document.getElementById('scaleMonth').onclick = () => { 
+      this.timeline.setOptions({ timeAxis: { scale: 'month', step: 1 }}); 
+    };
+
+    document.getElementById('scaleQuarter').onclick = () => { 
+      this.timeline.setOptions({ timeAxis: { scale: 'month', step: 3 }}); 
+    };
+
+    document.getElementById('scaleYear').onclick = () => { 
+      this.timeline.setOptions({ timeAxis: { scale: 'year', step: 1 }}); 
+    };
 
     this.timeline.on('select', (properties) => {
     });
